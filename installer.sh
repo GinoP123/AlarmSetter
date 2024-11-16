5425b182d355e4c0372fb77083780d93c806fe17
Gino Prasad <ginoprasad@gmail.com>
2024-11-15 23:17:53 -0800

    Updated installer

--- installer.sh
+++ installer.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+touch text_files/next_alarm.txt
+touch text_files/alarms.csv
+touch text_files/alarm_reminder.py
+
 if [[ $SHELL == *zsh ]]; then
 	if [[ "$(cat ~/.zprofile | grep AlarmSetter)" == "" ]]; then
-		echo "export PATH=\"\$PATH:$PWD/AlarmSetter/bin\"" >> ~/.zprofile
+		echo "export PATH=\"\$PATH:$PWD/bin\"" >> ~/.zprofile
 	fi
 else
 	if [[ "$(cat ~/.bashrc | grep AlarmSetter)" == "" ]]; then
-		echo "export PATH=\"\$PATH:$PWD/AlarmSetter/bin\"" >> ~/.bashrc
+		echo "export PATH=\"\$PATH:$PWD/bin\"" >> ~/.bashrc
 	fi
 fi
