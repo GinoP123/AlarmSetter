032eda16f740e88e2522d425315c031b2e8e365d
Gino Prasad <ginoprasad@gmail.com>
2024-11-29 12:08:48 -0800

    Updated settings to give range of file openers

--- settings.py
+++ settings.py
@@ -1,9 +1,13 @@
-import os
+import os, shutil
 
 next_alarm_file = f"{os.environ['HOME']}/Scripts/AlarmSetter/text_files/next_alarm.txt"
 alarms_file = f"{os.environ['HOME']}/Scripts/AlarmSetter/text_files/alarms.csv"
 alarm_reminder_file = f"{os.environ['HOME']}/Scripts/AlarmSetter/text_files/alarm_reminder.py"
 
 alarm_unset_script = f"{os.environ['HOME']}/Scripts/AlarmSetter/main.sh unset"
-open_file_script = "/usr/local/bin/sublime"
+
+open_file_script = shutil.which("subl")
+open_file_script = "/usr/local/bin/sublime" if not open_file_script else open_file_script
+open_file_script = shutil.which("vim") if not open_file_script else open_file_script
+
 crontab_file = f"{os.environ['HOME']}/Scripts/miscellaneous/text_files/crontab.txt"
